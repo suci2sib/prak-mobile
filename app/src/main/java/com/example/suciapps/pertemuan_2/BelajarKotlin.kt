7a8a0bc20a90668409024801f3f37bcaa9b172d0
suci2sib <ramadani24si@mahasiswa.pcr.ac.id>
2026-03-09 10:18:49 +0700

    first commit

---
+++ app/src/main/java/com/example/suciapps/pertemuan_2/BelajarKotlin.kt
@@ -0,0 +1,41 @@
+package com.example.suciapps.pertemuan_2
+fun main() {
+    println("Hai rekan-rekan...")
+    println("Selamat datang di bahasa pemrograman kotlin")
+
+    println("===================")
+
+    var angka = 15
+    println("Hasil sari 15 + 10 = ${angka + 10}")
+
+    val nilaiInt = 10000
+    val nilaiDouble = 100.003
+    val nilaiFloat = 1000.0f
+
+    println("Nilai Integer = $nilaiInt")
+    println("Nilai Double = $nilaiDouble")
+    println("Nilai Float = $nilaiFloat")
+
+    println("===========STRING========")
+
+    val nilaiString = "Mawar"
+    println("Hlo $nilaiString!\nApa Kabar?")
+
+    println("=========KONDISI=========")
+
+    val nilai = 10
+    if(nilai<0)
+        println("Bilangan negatif")
+    else {
+        if(nilai%2 == 0)
+            println("Bilangan genap")
+        else
+            println("Bilangan Ganjil")
+    }
+
+    println("========PERULANGAN=========")
+    val kampusKu: Array<String> = arrayOf("Kampus", "Politeknik", "Caltex", "Riau")
+    for (kampus: String in kampusKu) {
+        println(kampus)
+    }
+}
